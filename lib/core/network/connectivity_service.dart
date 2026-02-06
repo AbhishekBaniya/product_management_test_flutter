cf75c4f91d15d241e0b0b96adf524a7cee55898a
AbhishekBaniya <aban12134@gmail.com>
2026-02-06 11:39:59 +0530

    dead code remove

--- lib/core/network/connectivity_service.dart
+++ lib/core/network/connectivity_service.dart
@@ -3,28 +3,6 @@ import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'dart:async';
 
-/*class ConnectivityService {
-  final Connectivity connectivity;
-
-  ConnectivityService(this.connectivity);
-
-  Stream<bool> get connectionStream async* {
-    // 🔥 Emit initial state FIRST
-    final initial = await connectivity.checkConnectivity();
-    yield initial != ConnectivityResult.none;
-
-    // 🔥 Then listen for changes
-    await for (final result in connectivity.onConnectivityChanged) {
-      yield result != ConnectivityResult.none;
-    }
-  }
-
-  Future<bool> isOnline() async {
-    final result = await connectivity.checkConnectivity();
-    return result != ConnectivityResult.none;
-  }
-}*/
-
 class ConnectivityService {
   final Connectivity connectivity;
 
--- lib/features/products/presentation/pages/product_list_page.dart
+++ lib/features/products/presentation/pages/product_list_page.dart
@@ -3,77 +3,13 @@ import 'package:get/get.dart';
 import 'package:merchant_products_app/features/products/presentation/pages/product_detail_page.dart';
 import 'package:merchant_products_app/features/products/presentation/pages/product_form_page.dart';
 import '../../../../injection_container.dart';
-import '../../domain/entities/product_entity.dart';
 import '../controllers/product_list_controller.dart';
 
 import '../widgets/offline_banner.dart';
 import '../widgets/product_card.dart';
 
-/*class ProductListPage extends StatelessWidget {
-  const ProductListPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = Get.put(sl<ProductListController>());
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Products')),
-      body: Column(
-        children: [
-          const OfflineBanner(),
-          Expanded(
-            child: Obx(() {
-              if (controller.isLoading.value && controller.products.isEmpty) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              if (controller.isError.value) {
-                return Center(
-                  child: ElevatedButton(
-                    onPressed: controller.fetchProducts,
-                    child: const Text('Retry'),
-                  ),
-                );
-              }
-
-              return RefreshIndicator(
-                onRefresh: controller.refreshProducts,
-                child: ListView.builder(
-                  itemCount: controller.products.length + 1,
-                  itemBuilder: (context, index) {
-                    if (index < controller.products.length) {
-                      final Product product = controller.products[index];
-                      return GestureDetector(onTap: () => Get.to(() => ProductDetailPage(productId: product.id)),
-                          child: ProductCard(product: product));
-                    } else {
-                      if (controller.hasMore.value) {
-                        controller.fetchProducts(loadMore: true);
-                        return const Padding(
-                          padding: EdgeInsets.all(16),
-                          child: Center(child: CircularProgressIndicator()),
-                        );
-                      } else {
-                        return const SizedBox();
-                      }
-                    }
-                  },
-                ),
-              );
-            }),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.to(() => const ProductFormPage()),
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}*/
 class ProductListPage extends StatelessWidget {
    const ProductListPage({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
